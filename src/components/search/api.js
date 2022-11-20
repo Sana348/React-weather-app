2e8985cb769436d2aaf85c1c0fd27ba4892a4682
Sana Parveen <misana348@gmail.com>
2022-11-20 07:53:50 +0530

    first commit

---
+++ src/components/search/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "634d87aa7fmshf7b1569399dbdd7p1d9585jsn74b9c47f5be9",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "50e05632c770f87eae5f47369e53c7fb";
